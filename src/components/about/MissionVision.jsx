da4243291a69bc68d35082d4b8120e2632b7b1d5
wirovi07 <wilmarandresrojasvillafane@gmail.com>
2025-12-05 17:15:46 -0500

    Descripción de cambios

--- src/components/about/MissionVision.jsx
+++ src/components/about/MissionVision.jsx
@@ -7,17 +7,34 @@ export default function MissionVision() {
                 <div className="col-md-6 mb-4">
                     <h3 style={{ color: "#0074b8", fontWeight: "700" }}>Misión</h3>
                     <p>
-                        Brindar servicios integrales en salud visual con estándares de calidad,
-                        tecnología avanzada y talento humano especializado.
+                        LA SOCIEDAD REGIONAL DE CIRUGIA OCULAR S.A.S presta servicios integrales 
+                        de salud visual, garantizando elevados estándares de calidad por medio 
+                        de personal competente, equipos de alta tecnologia, un ambiente seguro y un 
+                        trato humanizado para los usuarios del Sur de los departamentos del Cesar 
+                        Magdalena Bolivar y sus áreas de influencia.
                     </p>
                 </div>
 
                 <div className="col-md-6 mb-4">
                     <h3 style={{ color: "#0074b8", fontWeight: "700" }}>Visión</h3>
                     <p>
-                        Ser una institución líder en oftalmología a nivel regional,
-                        reconocida por su excelencia y compromiso con la salud visual.
+                        LA SOCIEDAD REGIONAL DE CIRUGIA OCULAR S.A.S logrará un reconocimiento 
+                        nacional por la prestación de servicios integrales de salud visual con
+                        altos estándares de calidad, garantizando su sostenibilidad emprasarial
+                        a través de un elevado compromiso con la satisfacción de los usuarios 
+                        y la utilización eficiente de los recursos.
                     </p>
+                </div>
+
+                <div className="col-md-6 mb-4">
+                    <h3 style={{ color: "#0074b8", fontWeight: "700" }}>Principios</h3>
+                    <ul>
+                        <li>Flexibilidad al cambio.</li>
+                        <li>Trabajo en equipo.</li>
+                        <li>Liderazgo.</li>
+                        <li>Responsabilidad.</li>
+                        <li>Cultura de excelencia.</li>
+                    </ul>
                 </div>
             </div>
         </section>
